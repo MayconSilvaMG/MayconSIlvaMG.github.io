96b8bb5043612703f93d7d82b9373f77ab108aea
Maycon Daniel da Silva <MayconSilvaMG>
2023-07-10 16:38:52 -0300

    Subindo repositório local para o remoto

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
+/*const themeSelect = document.getElementById('themeSelect');
+themeSelect.addEventListener('change', switchTheme);*/
+
 
 button.addEventListener('click', function () {
   mobileNavbar.classList.toggle('active');
@@ -10,3 +13,11 @@ window.addEventListener('scroll', function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add('active');
   return navbar.classList.remove('active');
 });
+
+/*function switchTheme () {
+  debugger;
+  const themeSelect=
+  document.getElementById("themeSelect").value;
+            document.getElementsByTagName("meta")
+            [0].content = themeSelect;
+}*/
